87228516819be71d071025ea599f2ed50e58ada8
David2 <baotuyen111@gmail.com>
2023-02-01 23:35:33 +0700

    dienmay edit config

--- config.php
+++ config.php
@@ -3,4 +3,4 @@ define('PREFIX_DB_NAME', 'u262945054_');
 define('DB_PASSWORD', 'Batman@123');
 
 define('DB_HOST', 'localhost');
-define('PREFIX_WP_HOME', 'https://savyone.site/sitedemo/dienmay/');
+define('PREFIX_WP_HOME', 'https://savyone.site/sitedemo/');
