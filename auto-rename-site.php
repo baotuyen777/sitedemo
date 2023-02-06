de0b4092e7cd829f9e2467617b0ea8fa8d11ed45
Beckham <tuyenbv@vieted.com>
2023-02-06 18:02:33 +0700

    change site name

---
+++ auto-rename-site.php
@@ -0,0 +1,49 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//1. scan all directory
+$sites = scandir(__DIR__, SCANDIR_SORT_DESCENDING);
+foreach ($sites as $site) {
+    if (strpos($site, '_done')) {
+        $siteDir = __DIR__ . '/' . $site;
+//        $newSiteName = vn_to_str($site);
+//        $newSiteName = str_replace('Mẫu website ', '', $site);
+//        $newSiteName = str_replace('Mãu website ', '', $site);
+        $newSiteName = str_replace('_done', '', $site);
+        rename($siteDir, __DIR__ . '/' . $newSiteName);
+        echo $site;
+        echo $newSiteName;
+        echo '<hr/>';
+    }
+}
+
+
+function vn_to_str($str)
+{
+    $unicode = array(
+        'a' => 'á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ',
+        'd' => 'đ',
+        'e' => 'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
+        'i' => 'í|ì|ỉ|ĩ|ị',
+        'o' => 'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
+        'u' => 'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
+        'y' => 'ý|ỳ|ỷ|ỹ|ỵ',
+        'A' => 'Á|À|Ả|Ã|Ạ|Ă|Ắ|Ặ|Ằ|Ẳ|Ẵ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+        'D' => 'Đ',
+        'E' => 'É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+        'I' => 'Í|Ì|Ỉ|Ĩ|Ị',
+        'O' => 'Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+        'U' => 'Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+        'Y' => 'Ý|Ỳ|Ỷ|Ỹ|Ỵ',
+    );
+
+    foreach ($unicode as $nonUnicode => $uni) {
+        $str = preg_replace("/($uni)/i", $nonUnicode, $str);
+    }
+    $str = str_replace(' ', '-', $str);
+    $str = strtolower($str);
+    return $str;
+
+}
